356aec5d75cc2348d0d2569feabb3f5645f72ccd
Jakub Acs <acsjakub@localhost.localdomain>
2019-01-17 15:41:41 +0100

    Changes to throughput extraction scripts, now with command line argument support

--- tools/log_parser.py
+++ tools/log_parser.py
@@ -1,8 +1,10 @@
 
 import re
+import csv
 
 TIME_INDEX = 2
 BYTES_READ_INDEX = 7
+DESTINCTION_SIGN = 'driver-heartbeat'
 
 #!!!!! deprecated atm !!!!!!
 def extract_throughput_by_time(filename):
@@ -26,12 +28,17 @@ def extract_throughput_by_time(filename):
     return throughputs
 
 def extract_throughput_by_ms(filename):
+    """
+    Uses a tgen log file of one particular client to extract the throughputs.
+    :param filename: target tgen log file.
+    :return:
+    """
     throughputs = {}
 
     with open(filename, 'r') as f:
         for line in f:
 
-            if not 'driver-heartbeat' in line:
+            if not DESTINCTION_SIGN in line:
                 continue
 
             parts = line.split()
@@ -39,6 +46,22 @@ def extract_throughput_by_ms(filename):
             time = float(parts[TIME_INDEX])
             size = int(parts[BYTES_READ_INDEX].replace('bytes-read=',''))
 
+            #this sets the granularity of the timestamps to be 100 ms
             throughputs[int(time*10)] = size
 
     return throughputs
+
+def save_throughputs_to_csv_file(throughputs, filename, labels):
+    """
+    Saves previously extracted throughputs to csv file named filename.
+    Creates first line according to labels list.
+    :param throughputs: dictionary with extracted throughputs
+    :param filename: name of the target CSV file
+    :param labels: list of labels for target CSV file
+    :return:
+    """
+    with open(filename, 'w') as csv_f:
+        writer = csv.writer(csv_f)
+        writer.writerow(labels)
+        for key, value in throughputs.items():
+            writer.writerow([key, value])
